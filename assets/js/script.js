7359e06fa7618915df480306d16d1223d1656453
Luke Hillman <luke@Lukes-MacBook-Pro-2.local>
2022-06-25 18:51:43 -0700

    first draft of question object

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,31 @@
 console.log("I'm a JavaScript file linked to this page!");
+
+var questionBank = [
+    {
+        question: "Is 'falsy' truthy or falsy?",
+        choices: [
+            {choice: "Truthy", isCorrect: true},
+            {choice: "Falsy", isCorrect: false},
+            {choice: "Neither; it's NaN", isCorrect: false},
+            {choice: "Neither; it's undefined", isCorrect: false}
+        ],
+        shuffleChoices: function() {
+            console.log("shuffleChoices function");
+        },
+        audio: "./assets/sound/somefile.mp3",
+        logQuestion: function() {
+            console.log(this.question + " -- " + this.choices[0].choice + ", " + this.choices[1].choice + ", " + this.choices[2].choice + ", " + this.choices[3].choice);
+        },
+    }
+];
+
+
+
+
+
+
+
+
+
+
+
